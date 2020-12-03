d49668e6474c2c05ff5ccb5eb456ea4499194401
Gabriel Valeriano <gabrielvaleriano18@outlook.com>
2020-12-03 18:49:15 -0300

    use node-dijkstra

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const fs = require("fs");
 const path = require("path");
-
-const dijkstra = require("./dijkstra");
+const Graph = require("node-dijkstra");
 
 const FILE_PATH = path.join(__dirname, "Cities.txt");
-const START = 145;
-const FINISH = 146;
+const START = '145';
+const FINISH = '146';
 
 const main = () => {
   const fileData = fs.readFileSync(FILE_PATH).toString();
@@ -15,29 +14,8 @@ const main = () => {
     line.split(";").map((entry) => Number(entry))
   );
 
-  //renaming column headers
-  matrix[0] = matrix[0].map((header) => {
-    if (header === START) {
-      return "start";
-    } else if (header === FINISH) {
-      return "finish";
-    } else {
-      return header;
-    }
-  });
-  //renaming row headers
-  //starts from the first row with weight data
-  for (let i = 1; i < matrix.length; i++) {
-    if (matrix[i][0] === START) {
-      matrix[i][0] = "start";
-    } else if (matrix[i][0] === FINISH) {
-      matrix[i][0] = "finish";
-    }
-  }
-
   const headers = matrix[0].slice(1, matrix.length); // gets all row headers (node names), skipping 0
   var graph = {};
-  console.log(headers);
   //building graph adjecency list
   for (let i = 0; i < headers.length; i++) {
     const currentVertex = headers[i];
@@ -56,13 +34,13 @@ const main = () => {
     }
     graph[currentVertex] = currentVertexAdjList;
   }
-  graph.finish = {};
-
-  results = dijkstra(graph);
+  const route = new Graph(graph);
+  const results = route.path(START, FINISH, { cost: true });
   console.log(`Início: ${START}
 Destino: ${FINISH}
-Distância total:${results.distance}
+Distância total:${results.cost}
 Rota: ${results.path.join(" -> ")}`);
+  
 };
 
 main();
